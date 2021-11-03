bc5410356ec6ae6875977cb083070c427a3a9fae
Pascal Garber <pascal@artandcode.studio>
2021-11-03 15:56:01 +0100

    cleanup file structure

--- examples/tuxemon/src/resources.ts
+++ examples/tuxemon/src/resources.ts
@@ -4,7 +4,7 @@ import { AsepriteResource } from "@excaliburjs/plugin-aseprite/src/index";
 
 import scientist from "./assets/sprites/scientist/scientist.json";
 import scientistCharSet from "./assets/sprites/scientist/scientist.png";
-import map from './assets/example-city.tmx';
+import map from './assets/maps/example-city.tmx';
 
 let Resources = {
   scientist: new AsepriteResource(scientist, false, scientistCharSet),
